161a6924d81327abb313211a91964b137684dba7
Kobzarev Roman <krv2306@gmail.com>
2021-05-24 21:38:27 +0300

    add: add 1-3 task

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,25 +5,45 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+   
+    if (monthNumber < 1 || monthNumber > 12) {
+        return nil;
+    }
+    
+    return [[NSDateFormatter new] monthSymbols][monthNumber - 1];
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss'Z'"];
+    NSDate *parsedDate = [dateFormatter dateFromString:date];
+    
+    return [[NSCalendar currentCalendar] component:NSCalendarUnitDay fromDate:parsedDate];
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setLocale: [[NSLocale alloc] initWithLocaleIdentifier:@"ru_RU"]];
+    [dateFormatter setDateFormat:@"E"];
+       
+    return [[dateFormatter stringFromDate:date] capitalizedString];
+    
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
-    return NO;
+    NSCalendar *newCalendar = [NSCalendar currentCalendar];
+    NSDateComponents *currentDate = [newCalendar components:(NSCalendarUnitWeekOfYear | NSCalendarUnitYear) fromDate:[NSDate now]];
+    NSDateComponents *givenDate = [newCalendar components:(NSCalendarUnitWeekOfYear | NSCalendarUnitYear) fromDate:date];
+    
+    return (currentDate.year == givenDate.year) && (currentDate.weekOfYear == givenDate.weekOfYear);
 }
 
 @end
--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,25 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    int result = 0;
+      
+      if (ratingArray == nil || ratingArray.count < 3) {
+          return result;
+      }
+      
+      for (int i = 0; i < [ratingArray count] - 2; i++) {
+          for (int j = i; j < [ratingArray count] - 1; j++) {
+              for (int k = j; k < [ratingArray count]; k++) {
+                  if ((ratingArray[i].integerValue < ratingArray[j].integerValue &&
+                       ratingArray[j].integerValue < ratingArray[k].integerValue) ||
+                      (ratingArray[i].integerValue > ratingArray[j].integerValue &&
+                       ratingArray[j].integerValue > ratingArray[k].integerValue)) {
+                      result += 1;
+                  }
+              }
+          }
+      }
+      return result;
 }
 
 @end
